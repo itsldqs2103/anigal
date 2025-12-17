e25d93c23b248ac0fe95cbdfd35f713a949a4eba
NobleLight <itsldqs2103@gmail.com>
2025-12-17 08:45:31 +0700

    feat: add disable all drag events

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -2,6 +2,7 @@ import { Roboto_Flex } from "next/font/google";
 import "@/app/globals.css";
 import { NextIntlClientProvider } from 'next-intl';
 import Footer from "@/components/Footer";
+import LayoutScript from "@/components/LayoutScript";
 
 const robotoFlex = Roboto_Flex({
   subsets: ["latin", "vietnamese"],
@@ -11,14 +12,14 @@ export const metadata = {
   description: "AniGal - a gallery of stunning anime-style illustrations and artwork.",
 };
 
-export default async function RootLayout({ children }) {
-
+export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${robotoFlex.className} antialiased bg-base-300 text-base-content`}
     >
       <body>
+        <LayoutScript />
         <NextIntlClientProvider>
           {children}
           <Footer />
