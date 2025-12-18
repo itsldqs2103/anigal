9ce0c3fcecb376153198b5715146a1d8c7912a27
NobleLight <itsldqs2103@gmail.com>
2025-12-18 13:09:23 +0700

    feat: adjust locale switch

--- components/LocaleSwitch.js
+++ components/LocaleSwitch.js
@@ -25,11 +25,11 @@ export default function LocaleSwitch({ customClass }) {
     <>
       {locale === 'en' ? (
 
-        <Link href={parentPath} locale="en" className={customClass}>
+        <Link href={parentPath} locale="vi" className={customClass}>
           <span className="fi fi-us"></span>
         </Link>
       ) : (
-        <Link href={parentPath} locale="vi" className={customClass}>
+        <Link href={parentPath} locale="en" className={customClass}>
           <span className="fi fi-vn"></span>
         </Link>
       )}
