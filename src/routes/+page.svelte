2ba37f92886129cfad2b928f6d2afd9194c5521b
itsldqs2103 <itsldqs2103@gmail.com>
2026-08-29 21:13:20 +0700

    chore: fix css and tailwindcss conflict

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -311,7 +311,7 @@
 							<img
 								data-lazyload-src={image.thumbnail}
 								alt="Image {image.id}"
-								class="lazyload h-full w-full object-cover transition duration-300 group-hover:scale-[1.02]"
+								class="lazyload h-full w-full object-cover group-hover:scale-[1.02]"
 							/>
 							<div
 								class="absolute inset-x-0 bottom-0 hidden items-end justify-end bg-linear-to-t from-black/60 to-transparent p-3 pt-10 opacity-0 transition group-hover:opacity-100 sm:flex"
