3f872bd71d3ad59b0be127923fc5aecd59707631
itsldqs2103 <itsldqs2103@gmail.com>
2026-08-31 10:34:04 +0700

    chore: update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -30,20 +30,33 @@ export default defineConfig({
 				display: 'standalone',
 				icons: [
 					{
-						src: '/pwa-192x192.png',
+						src: '/icons/pwa-192x192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: '/pwa-512x512.png',
+						src: '/icons/pwa-512x512.png',
 						sizes: '512x512',
 						type: 'image/png'
 					},
 					{
-						src: '/maskable-icon-512x512.png',
+						src: '/icons/maskable-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'maskable'
+					}
+				],
+				"screenshots": [
+					{
+						"src": "/screenshots/desktop.png",
+						"sizes": "1911x1007",
+						"type": "image/png",
+						"form_factor": "wide"
+					},
+					{
+						"src": "/screenshots/mobile.png",
+						"sizes": "800x2000",
+						"type": "image/png"
 					}
 				]
 			},
