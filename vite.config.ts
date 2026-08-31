95ca52c9d0cb9f87e65e103f3f38072c4f4c89e6
itsldqs2103 <itsldqs2103@gmail.com>
2026-08-31 09:24:47 +0700

    chore: add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
